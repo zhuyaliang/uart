f880102df8cb0ff1109f0bd6ea645c6f8a7e2696
zhuyaliang <15132211195@163.com>
2018-06-08 18:33:10 +0800

    face complete

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <sys/stat.h>
+#include <fcntl.h>
 #include<gtk/gtk.h>
 #include<glib.h>
 #include<stdio.h>
@@ -9,6 +11,10 @@
 #include <vte-2.91/vte/vte.h>
 #include <sys/stat.h>
 #include<gdk/gdkkeysyms.h>
+
+#define     OPEN     1
+#define     CLOSE    0
+
 
 typedef struct 
 {
@@ -34,7 +40,20 @@ typedef struct
 	
 }UartControl;
 
+int gnSwitchSatue;
+void SetWidgetStyle(GtkWidget * Widget,const char *Color,int FontSize)
+{
+    GdkRGBA Rgb;
+    PangoFontDescription *font_desc;
 
+    if(gdk_rgba_parse(&Rgb,Color) == TRUE)
+        gtk_widget_override_color(Widget, GTK_STATE_NORMAL, &Rgb);
+
+    font_desc = pango_font_description_from_string("Sans Blod 20");  
+    pango_font_description_set_size (font_desc, FontSize * PANGO_SCALE);
+    gtk_widget_modify_font (Widget, font_desc);
+    pango_font_description_free (font_desc);
+}
 void SetLableFontType(GtkWidget *Lable ,const char *Color,int FontSzie,const char *Word)
 {
     char LableTypeBuf[200] = { 0 };
@@ -56,7 +75,7 @@ GtkWidget *CreateMainWindow(void)
 	gtk_window_set_position(GTK_WINDOW(WindowMain), GTK_WIN_POS_CENTER);
 	gtk_widget_realize(WindowMain);
     gtk_container_set_border_width(GTK_CONTAINER(WindowMain),10);
-	gtk_widget_set_size_request(WindowMain, 500, 500);
+	gtk_widget_set_size_request(WindowMain, 600, 500);
     pixbuf = gdk_pixbuf_new_from_file ("xgcom.png", &error);
     gtk_window_set_icon (GTK_WINDOW (WindowMain), pixbuf);
 	
@@ -174,11 +193,33 @@ GtkWidget *SetData(void)
 
     return SetComboBox(Store);
 }
-static void OpenSerial(GtkWidget *togglebutton,gpointer user_data)
-{
-      printf("aaaaaaaaaaaaaa\r\n");
-//    gtk_toggle_button_set_active(togglebutton,TRUE);
 
+int GetSwitchState( const char *ButtonName)
+{
+    if(strstr(ButtonName,"Close") != NULL)
+        return 0;
+    else
+        return 1;
+
+}        
+static void OpenSerial(GtkWidget *Button,gpointer user_data)
+{
+    int SwitchState;
+
+    SwitchState = GetSwitchState(gtk_button_get_label(GTK_BUTTON(Button)));
+    if(SwitchState == CLOSE)
+    {
+        gtk_button_set_label(GTK_BUTTON(Button),"● Open");
+        SetWidgetStyle(Button,"black",13);
+    
+    }
+    else
+    {        
+        gtk_button_set_label(GTK_BUTTON(Button),"● Close");
+        SetWidgetStyle(Button,"red",13);
+    }    
+
+    printf("aaaaaaaaaaaaaa\r\n");
 }        
 void SerialPortSetup(GtkWidget *Hbox)
 {
@@ -198,16 +239,16 @@ void SerialPortSetup(GtkWidget *Hbox)
 	GtkWidget *Hseparator;
 	GtkWidget *Button;
 
-
 	Vpaned = gtk_paned_new (GTK_ORIENTATION_VERTICAL);
 	gtk_container_add (GTK_CONTAINER (Hbox), Vpaned);
-	Frame = gtk_frame_new ("Serial Setting");
+	
+    Frame = gtk_frame_new ("Serial Setting");
+    SetWidgetStyle(Frame,"black",12);
 	gtk_frame_set_label_align(GTK_FRAME(Frame),0.5,0.3);
-	gtk_widget_show (Frame);
 	gtk_paned_pack1 (GTK_PANED (Vpaned), Frame, FALSE, TRUE);
 	
     Table = gtk_grid_new();
-    gtk_container_add (GTK_CONTAINER (Frame), Table);
+    gtk_container_add (GTK_CONTAINER (Vpaned), Table);
 	gtk_grid_set_column_homogeneous(GTK_GRID(Table),TRUE);
 
     LableComPort = gtk_label_new(NULL);
@@ -250,11 +291,10 @@ void SerialPortSetup(GtkWidget *Hbox)
     gtk_combo_box_set_active(GTK_COMBO_BOX(SelectStop),1);
     gtk_grid_attach(GTK_GRID(Table) ,SelectStop , 1 , 4 , 1 , 1);
    
-    Button = gtk_toggle_button_new_with_label("open");
+    Button = gtk_button_new_with_label ("● open");
+    SetWidgetStyle(Button,"black",13);
     gtk_grid_attach(GTK_GRID(Table) , Button , 0 , 5 , 2 , 1);
-    g_signal_connect_swapped(G_OBJECT(Button), "toggled", G_CALLBACK(OpenSerial), NULL);  
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(Button),TRUE);
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(Button),FALSE);
+    g_signal_connect(G_OBJECT(Button), "clicked", G_CALLBACK(OpenSerial), NULL);  
      
 	Hseparator = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
     gtk_grid_attach(GTK_GRID(Table) , Hseparator , 0 , 7 , 2 , 1);
@@ -271,7 +311,7 @@ void ReceiveSet(GtkWidget *Hbox)
 	GtkWidget *CheckStop;
 	GtkWidget *Hseparator;
 	GtkWidget *Vpaned;
-	GtkWidget *rcv_frame;
+	GtkWidget *RcvFrame;
     GtkWidget *LabelSave;
     GtkWidget *LabelClear;
 	GtkWidget *LabelSpace;
@@ -279,13 +319,13 @@ void ReceiveSet(GtkWidget *Hbox)
 	
 	Vpaned = gtk_paned_new (GTK_ORIENTATION_VERTICAL);
 	gtk_container_add (GTK_CONTAINER (Hbox), Vpaned);
-	rcv_frame = gtk_frame_new ("Receive Setting");
-	gtk_frame_set_label_align(GTK_FRAME(rcv_frame),0.5,0.3);
-	gtk_widget_show (rcv_frame);
-	gtk_paned_pack1 (GTK_PANED (Vpaned), rcv_frame, FALSE, TRUE);
+	RcvFrame = gtk_frame_new ("Receive Setting");
+	gtk_frame_set_label_align(GTK_FRAME(RcvFrame),0.5,0.3);
+    SetWidgetStyle(RcvFrame,"black",12);
+	gtk_paned_pack1 (GTK_PANED (Vpaned), RcvFrame, FALSE, TRUE);
 	
 	Table = gtk_grid_new();
-	gtk_container_add (GTK_CONTAINER (rcv_frame), Table);
+    gtk_container_add (GTK_CONTAINER (Vpaned), Table);
 	gtk_grid_set_column_homogeneous(GTK_GRID(Table),TRUE);
 
 	LabelSpace = gtk_label_new(" ");
@@ -336,11 +376,11 @@ void SendSet(GtkWidget *Hbox)
 	gtk_container_add (GTK_CONTAINER (Hbox), Vpaned);
 	SendFrame = gtk_frame_new ("Send Setting");
 	gtk_frame_set_label_align(GTK_FRAME(SendFrame),0.5,0.3);
-	gtk_widget_show (SendFrame);
+    SetWidgetStyle(SendFrame,"black",12);
 	gtk_paned_pack1 (GTK_PANED (Vpaned), SendFrame, FALSE, TRUE);
 	
 	Table = gtk_grid_new();
-	gtk_container_add (GTK_CONTAINER (SendFrame), Table);
+    gtk_container_add (GTK_CONTAINER (Vpaned), Table);
 	gtk_grid_set_column_homogeneous(GTK_GRID(Table),TRUE);
 	
 	LabelSpace = gtk_label_new(" ");
@@ -359,6 +399,8 @@ void SendSet(GtkWidget *Hbox)
     gtk_grid_attach(GTK_GRID(Table) , CheckStop , 0 , 4, 2 , 1);
     
     EntrySendCycle = gtk_entry_new();
+    gtk_widget_set_size_request (EntrySendCycle, 40, -1);
+    gtk_entry_set_invisible_char (GTK_ENTRY (EntrySendCycle), 9679);
     gtk_entry_set_max_length(GTK_ENTRY(EntrySendCycle),6);
     gtk_entry_set_text(GTK_ENTRY(EntrySendCycle),"1000");
     gtk_grid_attach(GTK_GRID(Table) , EntrySendCycle , 0 , 5, 2 , 1);
@@ -390,85 +432,124 @@ GtkWidget *CreateUartSetFace(GtkWidget *Hbox,UartSetFace *ULC)
 	
 }
 
+void CreateReceiveFace(GtkWidget *Vpaned)
+{
+    GtkWidget *ReveTerminal;
+	GtkWidget *Frame;
+	GtkWidget *Scrolled;
+
+	Frame = gtk_frame_new ("Receive Data From Serial");
+	gtk_frame_set_label_align(GTK_FRAME(Frame),0.5,0.3);
+	gtk_paned_pack1 (GTK_PANED (Vpaned),Frame, FALSE, TRUE);
+	gtk_widget_set_size_request (Frame, -1, 430);
+
+	Scrolled = gtk_scrolled_window_new (NULL, NULL);
+	gtk_container_add (GTK_CONTAINER (Frame), Scrolled);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (Scrolled), 
+                                    GTK_POLICY_NEVER, 
+                                    GTK_POLICY_AUTOMATIC);
+	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (Scrolled), 
+                                         GTK_SHADOW_IN);
+
+	ReveTerminal = vte_terminal_new();
+	
+    vte_terminal_set_backspace_binding(VTE_TERMINAL(ReveTerminal),
+				                       VTE_ERASE_ASCII_BACKSPACE);
+    vte_terminal_set_input_enabled(VTE_TERMINAL(ReveTerminal),1);
+    vte_terminal_set_scroll_on_output(VTE_TERMINAL(ReveTerminal),TRUE);
+    vte_terminal_set_mouse_autohide(VTE_TERMINAL(ReveTerminal),1);
+/*
+    int fd;
+    char buf[10240] = { 0 };
+    fd = open("./main.c",O_RDONLY);
+    read(fd,buf,10240);
+	vte_terminal_feed(VTE_TERMINAL(ReveTerminal), buf, strlen(buf));
+*/    gtk_container_add (GTK_CONTAINER (Scrolled), ReveTerminal);
+
+
+}
+
+void CreateSendFace(GtkWidget *Vpaned)
+{
+    GtkWidget *Frame;
+	GtkWidget *Scrolled;
+	GtkWidget *SendText;
+
+	Frame = gtk_frame_new ("Send Data Serial");
+	gtk_frame_set_label_align(GTK_FRAME(Frame),0.5,0.3);
+	gtk_paned_pack2 (GTK_PANED (Vpaned), Frame, FALSE, TRUE);
+	gtk_widget_set_size_request (Frame, -1, 154);
+    
+    Scrolled = gtk_scrolled_window_new (NULL, NULL);
+	gtk_container_add (GTK_CONTAINER (Frame), Scrolled);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (Scrolled), 
+                                    GTK_POLICY_NEVER, 
+                                    GTK_POLICY_AUTOMATIC);
+	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (Scrolled),
+                                         GTK_SHADOW_IN);
+
+	SendText = gtk_text_view_new ();
+	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (SendText), GTK_WRAP_CHAR);
+	gtk_container_add (GTK_CONTAINER (Scrolled), SendText);
+
+
+}
+
 GtkWidget *CreateUartTextFace(GtkWidget *Hbox,UartTextFace *URC)
 {
-	GtkWidget *right_vbox;
-	GtkWidget *rcv_text;
-	GtkWidget *right_vpaned;
-	GtkWidget *rcv_frame;
-	GtkWidget *alignment2;
-	GtkWidget *scrolledwindow2;
-	GtkWidget *rcv_label;
-	GtkWidget *send_frame;
-	GtkWidget *alignment1;
-	GtkWidget *scrolledwindow1;
-	GtkWidget *send_text;
-	GtkWidget *send_label;
+	GtkWidget *Vbox;
+	GtkWidget *Vpaned;
 	
-	right_vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-	gtk_widget_show (right_vbox);
-	gtk_box_pack_start (GTK_BOX (Hbox), right_vbox, FALSE, FALSE, 0);
-	gtk_widget_set_size_request (right_vbox, 500, -1);
-
-	right_vpaned = gtk_paned_new (GTK_ORIENTATION_VERTICAL);
-	gtk_widget_show (right_vpaned);
-	gtk_container_add (GTK_CONTAINER (right_vbox), right_vpaned);
+	Vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+	gtk_box_pack_start (GTK_BOX (Hbox), Vbox, FALSE, FALSE, 0);
+	gtk_widget_set_size_request (Vbox, 470,-1);
+	Vpaned = gtk_paned_new (GTK_ORIENTATION_VERTICAL);
+	gtk_container_add (GTK_CONTAINER (Vbox), Vpaned);
   
-	rcv_frame = gtk_frame_new (NULL);
-	gtk_widget_show (rcv_frame);
-	//gtk_box_pack_start (GTK_BOX (right_vbox), rcv_frame, TRUE, TRUE, 0);
-	gtk_paned_pack1 (GTK_PANED (right_vpaned), rcv_frame, FALSE, TRUE);
-	gtk_widget_set_size_request (rcv_frame, -1, 380);
-
-	alignment2 = gtk_alignment_new (0.5, 0.5, 1, 1);
-	gtk_widget_show (alignment2);
-	gtk_container_add (GTK_CONTAINER (rcv_frame), alignment2);
-
-	scrolledwindow2 = gtk_scrolled_window_new (NULL, NULL);
-	gtk_widget_show (scrolledwindow2);
-	gtk_container_add (GTK_CONTAINER (alignment2), scrolledwindow2);
-	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow2),  GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolledwindow2), GTK_SHADOW_IN);
-/*
-	rcv_text = vte_terminal_new();
-	vte_terminal_set_backspace_binding(VTE_TERMINAL(rcv_text),
-				     VTE_ERASE_ASCII_BACKSPACE);
-	gtk_widget_show (rcv_text);
-	gtk_container_add (GTK_CONTAINER (scrolledwindow2), rcv_text);
-	
-	rcv_label = gtk_label_new (("Receive:"));
-	gtk_widget_show (rcv_label);
-	gtk_frame_set_label_widget (GTK_FRAME (rcv_frame), rcv_label);
-	gtk_label_set_use_markup (GTK_LABEL (rcv_label), TRUE);
-
-	send_frame = gtk_frame_new (NULL);
-	gtk_widget_show (send_frame);
-	//gtk_box_pack_start (GTK_BOX (right_vbox), send_frame, TRUE, TRUE, 0);
-	gtk_paned_pack2 (GTK_PANED (right_vpaned), send_frame, FALSE, TRUE);
-
-	alignment1 = gtk_alignment_new (0.5, 0.5, 1, 1);
-	gtk_widget_show (alignment1);
-	gtk_container_add (GTK_CONTAINER (send_frame), alignment1);
-
-	scrolledwindow1 = gtk_scrolled_window_new (NULL, NULL);
-	gtk_widget_show (scrolledwindow1);
-	gtk_container_add (GTK_CONTAINER (alignment1), scrolledwindow1);
-	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow1),  GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolledwindow1), GTK_SHADOW_IN);
-
-	send_text = gtk_text_view_new ();
-	gtk_widget_show (send_text);
-	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (send_text), GTK_WRAP_CHAR);
-	gtk_container_add (GTK_CONTAINER (scrolledwindow1), send_text);
-	
-	send_label = gtk_label_new (("Send:"));
-	gtk_widget_show (send_label);
-	gtk_frame_set_label_widget (GTK_FRAME (send_frame), send_label);
-	gtk_label_set_use_markup (GTK_LABEL (send_label), TRUE);
-*/	
-	return right_vbox;
+    CreateReceiveFace(Vpaned);
+    
+    CreateSendFace(Vpaned);
+        
+    return Vbox;
 }
-	
+void CreateUartBottom(GtkWidget *Vbox)
+{
+    GtkWidget *image;
+    GtkWidget *LabelState;
+    GtkWidget *LabelRx;
+    GtkWidget *LabelTx;
+    GtkWidget *LabelSpace;
+
+    GtkWidget *Button;
+    GtkWidget *Hbox;
+
+    Hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);
+    gtk_box_pack_start (GTK_BOX (Vbox), Hbox, FALSE, FALSE, 0);
+
+    image = gtk_image_new_from_icon_name ("audio-speakers",
+                                           GTK_ICON_SIZE_MENU);
+    gtk_box_pack_start (GTK_BOX (Hbox), image, FALSE, FALSE, 0);
+    LabelState = gtk_label_new("Serial Assistant");
+    gtk_box_pack_start (GTK_BOX (Hbox), LabelState, TRUE, FALSE, 0);
+
+    LabelSpace = gtk_label_new("           ");
+    gtk_box_pack_start (GTK_BOX (Hbox), LabelSpace, TRUE, TRUE, 0);
+
+    LabelRx = gtk_toggle_button_new_with_label("RX:    ");
+    gtk_box_pack_start (GTK_BOX (Hbox), LabelRx, FALSE, FALSE, 0);
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(LabelRx),TRUE);
+    SetWidgetStyle(LabelRx,"blue",11);
+
+    LabelTx = gtk_toggle_button_new_with_label("TX:    ");
+    gtk_box_pack_start (GTK_BOX (Hbox), LabelTx, TRUE, FALSE, 0);
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(LabelTx),TRUE);
+    SetWidgetStyle(LabelTx,"blue",11);
+    
+    Button = gtk_button_new_with_label ("Send");
+    SetWidgetStyle(Button,"black",11);
+    gtk_box_pack_start (GTK_BOX (Hbox), Button, TRUE, FALSE, 0);
+
+}        
 
 void CreateFace (GtkWidget *Vbox)
 {
@@ -478,10 +559,11 @@ void CreateFace (GtkWidget *Vbox)
 	GtkWidget *RightTextBox;
 	UartControl uc;
 	
-	Hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);	
+	Hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);	
     gtk_box_pack_start (GTK_BOX (Vbox), Hbox, FALSE, FALSE, 0);
    	LeftSetBox = CreateUartSetFace(Hbox,uc.ULC);
 	RightTextBox = CreateUartTextFace(Hbox,uc.URC);
+    CreateUartBottom(Vbox);
 	
 }
 int main(int argc, char **argv)
@@ -489,10 +571,15 @@ int main(int argc, char **argv)
 	GtkWidget *MainWindow;
 	GtkWidget *Vbox;
 	UartControl uc;
-	 
     gtk_init(&argc, &argv);
    
     MainWindow = CreateMainWindow();
+
+    GdkPixbuf *Xgcom_icon_pixbuf;
+    GError *error = NULL;
+    Xgcom_icon_pixbuf = gdk_pixbuf_new_from_file ("xgcom.png", &error);
+    gtk_window_set_icon (GTK_WINDOW (MainWindow), Xgcom_icon_pixbuf);
+
     Vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL,0);	
 	gtk_container_add (GTK_CONTAINER (MainWindow), Vbox);
     uc.MainWindow = MainWindow;
